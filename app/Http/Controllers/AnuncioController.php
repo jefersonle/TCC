7f8fa45916708ed976e457be981f89548a0ab1ee
jefersonle <jeferson_eurides@hotmail.com>
2016-10-24 21:37:32 -0200

    Upload Imagens

--- app/Http/Controllers/AnuncioController.php
+++ app/Http/Controllers/AnuncioController.php
@@ -1,125 +1,263 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
-use App\Models\Anuncio;
-use App\Models\Estado;
-
-class AnuncioController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function __construct()
-    {
-        $this->middleware('auth', ['only' => ['getCreate', 'postCreate', 'getEdit', 'postUpdate', 'getDestroy']]);
-        
-    }
-    public function getIndex()
-    {
-        $anuncios = Anuncio::all();
-        $data['anuncios'] = $anuncios;
-        $data['bread'] = "Todos os anúncios";
-        return view('anuncio', $data);
-    }
-
-    
-    public function getCreate()
-    {
-        $data['tipo'] = 'criar';
-        return view('formanuncio', $data);
-
-    }
-
-    
-    public function postCreate(Request $request)
-    {
-        $anuncio = new Anuncio();
-        $anuncio->cidade_id = $request->cidade_id;
-        $anuncio->user_id = 1;
-        $anuncio->categoria_id = $request->categoria_id;
-        $anuncio->titulo = $request->titulo;
-        $anuncio->descricao = $request->descricao;
-        $anuncio->valor = $request->valor;
-        $anuncio->save();
-
-        return redirect('/anuncio/create'); 
-
-    }
-
-    
-    public function getShow($id)
-    {
-        $anuncio = Anuncio::find($id);
-        $data['anuncio'] = $anuncio;
-        return view('single', $data);
-    }
-
-    
-    public function getEdit($id)
-    {
-        $anuncio = Anuncio::find($id);
-        $data['anuncio'] = $anuncio;
-        $data['tipo'] = 'editar';
-        // return view('formanuncio', $data);
-
-    }
-
-   
-    public function postUpdate(Request $request, $id)
-    {
-        $anuncio = Anuncio::find($id);
-        $anuncio->cidade_id = $request->cidade_id;
-        $anuncio->user_id = $request->user_id;
-        $anuncio->categoria_id = $request->categoria_id;
-        $anuncio->titulo = $request->titulo;
-        $anuncio->descricao = $request->descricao;
-        $anuncio->valor = $request->valor;
-        $anuncio->save();
-
-        return redirect('/anuncio');   
-    }
-
-    
-    public function getDestroy($id)
-    {
-        $anuncio = Anuncio::find($id);
-        $anuncio->delete();
-
-        return redirect('/anuncio');
-    }
-
-     public function getCidade($id)
-    {
-        $anuncios = Anuncio::where('cidade_id', $id)->orderBy('created_at')->get();
-        $data['anuncios'] = $anuncios;
-        $data['bread'] = $anuncios[0]->cidade->nome;
-        return view('anuncio', $data);
-        
-    }
-
-    public function getEstado($id)
-    {
-        $anuncios = Estado::find($id)->anuncios()->get();        
-        $data['anuncios'] = $anuncios;
-        $data['bread'] = $anuncios[0]->cidade->estado->nome;
-        return view('anuncio', $data);
-        
-    }
-
-    public function getCategoria($id)
-    {
-        $anuncios = Anuncio::where('categoria_id', $id)->get();        
-        $data['anuncios'] = $anuncios;
-        $data['bread'] = $anuncios[0]->categoria->nome;
-        return view('anuncio', $data);
-        
-    }
-}
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\Anuncio;
+
+use App\Models\Estado;
+use App\Models\Imagem;
+
+
+
+class AnuncioController extends Controller
+
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+
+    {
+
+        $this->middleware('auth', ['only' => ['getCreate', 'postCreate', 'getEdit', 'postUpdate', 'getDestroy']]);
+
+        
+
+    }
+
+    public function getIndex()
+
+    {
+
+        $anuncios = Anuncio::all();
+
+        $data['anuncios'] = $anuncios;
+
+        $data['bread'] = "Todos os anúncios";
+
+        return view('anuncio', $data);
+
+    }
+
+
+
+    
+
+    public function getCreate()
+
+    {
+
+        $data['tipo'] = 'criar';
+
+        return view('formanuncio', $data);
+
+
+
+    }
+
+
+
+    
+
+    public function postCreate(Request $request)
+
+    {   
+
+        $anuncio = new Anuncio();
+
+        $anuncio->cidade_id = $request->cidade_id;
+
+        $anuncio->user_id = 1;
+
+        $anuncio->categoria_id = $request->categoria_id;
+
+        $anuncio->titulo = $request->titulo;
+
+        $anuncio->descricao = $request->descricao;
+
+        $anuncio->valor = $request->valor;
+
+        $anuncio->save();
+
+        $path = "uploads";
+        
+        if ($request->hasFile('images')) {  
+            $files = $request->files;         
+            foreach($files as $images){              
+                foreach ($images as $image) {
+                    $name = $image->getClientOriginalName();
+                    $name = microtime() . "_" . $name;
+                    $image->move($path, $name);
+
+                    $imagem = new Imagem();
+                    $imagem->anuncio_id = $anuncio->id;
+                    $imagem->nome = $name;
+                    $imagem->save();
+
+                }
+            }
+               
+        }        
+
+
+        
+        
+
+
+        return redirect('/anuncio/create'); 
+
+
+    }
+
+
+
+    
+
+    public function getShow($id)
+
+    {
+
+        $anuncio = Anuncio::find($id);
+
+        $data['anuncio'] = $anuncio;
+
+        return view('single', $data);
+
+    }
+
+
+
+    
+
+    public function getEdit($id)
+
+    {
+
+        $anuncio = Anuncio::find($id);
+
+        $data['anuncio'] = $anuncio;
+
+        $data['tipo'] = 'editar';
+
+        // return view('formanuncio', $data);
+
+
+
+    }
+
+
+
+   
+
+    public function postUpdate(Request $request, $id)
+
+    {
+
+        $anuncio = Anuncio::find($id);
+
+        $anuncio->cidade_id = $request->cidade_id;
+
+        $anuncio->user_id = $request->user_id;
+
+        $anuncio->categoria_id = $request->categoria_id;
+
+        $anuncio->titulo = $request->titulo;
+
+        $anuncio->descricao = $request->descricao;
+
+        $anuncio->valor = $request->valor;
+
+        $anuncio->save();
+
+
+
+        return redirect('/anuncio');   
+
+    }
+
+
+
+    
+
+    public function getDestroy($id)
+
+    {
+
+        $anuncio = Anuncio::find($id);
+
+        $anuncio->delete();
+
+
+
+        return redirect('/anuncio');
+
+    }
+
+
+
+     public function getCidade($id)
+
+    {
+
+        $anuncios = Anuncio::where('cidade_id', $id)->orderBy('created_at')->get();
+
+        $data['anuncios'] = $anuncios;
+
+        $data['bread'] = $anuncios[0]->cidade->nome;
+
+        return view('anuncio', $data);
+
+        
+
+    }
+
+
+
+    public function getEstado($id)
+
+    {
+
+        $anuncios = Estado::find($id)->anuncios()->get();        
+
+        $data['anuncios'] = $anuncios;
+
+        $data['bread'] = $anuncios[0]->cidade->estado->nome;
+
+        return view('anuncio', $data);
+
+        
+
+    }
+
+
+
+    public function getCategoria($id)
+
+    {
+
+        $anuncios = Anuncio::where('categoria_id', $id)->get();        
+
+        $data['anuncios'] = $anuncios;
+
+        $data['bread'] = $anuncios[0]->categoria->nome;
+
+        return view('anuncio', $data);
+
+        
+
+    }
+
+}
+
--- resources/views/anuncio.blade.php
+++ resources/views/anuncio.blade.php
@@ -1,254 +1,511 @@
-@extends('layouts.portal')
-
-@section('scripts')	
-<!-- js -->
-<script type="text/javascript" src="{{ url('/') }}/js/jquery.min.js"></script>
-<!-- js -->
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="{{ url('/') }}/js/bootstrap.min.js"></script>
-<script src="{{ url('/') }}/js/bootstrap-select.js"></script>
-<script>
-  $(document).ready(function () {
-    var mySelect = $('#first-disabled2');
-
-    $('#special').on('click', function () {
-      mySelect.find('option:selected').prop('disabled', true);
-      mySelect.selectpicker('refresh');
-    });
-
-    $('#special2').on('click', function () {
-      mySelect.find('option:disabled').prop('disabled', false);
-      mySelect.selectpicker('refresh');
-    });
-
-    $('#basic2').selectpicker({
-      liveSearch: true,
-      maxOptions: 1
-    });
-  });
-</script>
-<script type="text/javascript" src="{{ url('/') }}/js/jquery.leanModal.min.js"></script>
-<link href="{{ url('/') }}/css/jquery.uls.css" rel="stylesheet"/>
-<link href="{{ url('/') }}/css/jquery.uls.grid.css" rel="stylesheet"/>
-<link href="{{ url('/') }}/css/jquery.uls.lcd.css" rel="stylesheet"/>
-<!-- Source -->
-<script src="{{ url('/') }}/js/jquery.uls.data.js"></script>
-<script src="{{ url('/') }}/js/jquery.uls.data.utils.js"></script>
-<script src="{{ url('/') }}/js/jquery.uls.lcd.js"></script>
-<script src="{{ url('/') }}/js/jquery.uls.languagefilter.js"></script>
-<script src="{{ url('/') }}/js/jquery.uls.regionfilter.js"></script>
-<script src="{{ url('/') }}/js/jquery.uls.core.js"></script>
-<script>
-			$( document ).ready( function() {
-				$( '.uls-trigger' ).uls( {
-					onSelect : function( language ) {
-						var languageName = $.uls.data.getAutonym( language );
-						$( '.uls-trigger' ).text( languageName );
-					},
-					quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
-				} );
-			} );
-		</script>
-    <script src="{{ url('/') }}/js/tabs.js"></script>
-	
-<script type="text/javascript">
-$(document).ready(function () {    
-var elem=$('#container ul');      
-	$('#viewcontrols a').on('click',function(e) {
-		if ($(this).hasClass('gridview')) {
-			elem.fadeOut(1000, function () {
-				$('#container ul').removeClass('list').addClass('grid');
-				$('#viewcontrols').removeClass('view-controls-list').addClass('view-controls-grid');
-				$('#viewcontrols .gridview').addClass('active');
-				$('#viewcontrols .listview').removeClass('active');
-				elem.fadeIn(1000);
-			});						
-		}
-		else if($(this).hasClass('listview')) {
-			elem.fadeOut(1000, function () {
-				$('#container ul').removeClass('grid').addClass('list');
-				$('#viewcontrols').removeClass('view-controls-grid').addClass('view-controls-list');
-				$('#viewcontrols .gridview').removeClass('active');
-				$('#viewcontrols .listview').addClass('active');
-				elem.fadeIn(1000);
-			});									
-		}
-	});
-});
-</script>
-
-@endsection
-
-@section('banner')
-	<div class="banner text-center">
-	  <div class="container">    
-			<h1>Lorem ipsum   <span class="segment-heading">    dolor amet </span> lorem ipsum</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			<a href="{{ url('/anuncio/create') }}">Criar Anúncio</a>
-	  </div>
-	</div>
-@endsection
-
-@section('content')
-	<!-- Mobiles -->
-	<div class="total-ads main-grid-border">
-		<div class="container">
-			<div class="select-box">
-				<div width="100%">
-					<div class="search" width="100%">
-						<div id="custom-search-input" width="100%">
-						<div class="input-group" width="100%">
-							<input type="text" class="form-control input-lg" placeholder="Buscar" width="100%" />
-							<span class="input-group-btn">
-								<button class="btn btn-info btn-lg" type="button">
-									<i class="glyphicon glyphicon-search"></i>
-								</button>
-							</span>
-						</div>
-					</div>
-					</div>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-			<ol class="breadcrumb" style="margin-bottom: 5px;">
-			  <li><a href="{{ url('/') }}">Home</a></li>
-			  <li><a href="{{ url('/categoria') }}">Categorias</a></li>
-			  <li class="active">
-			     @if(isset($bread))
-			         {{ $bread }}
-			     @endif
-			      
-			  </li>
-			</ol>
-			<div class="ads-grid">
-				<div class="side-bar col-md-3">	
-				<div class="featured-ads">
-					<h2 class="sear-head fer">Anúncios Relacionados</h2>
-					<div class="featured-ad">
-						<a href="single.html">
-							<div class="featured-ad-left">
-								<img src="{{ url('/') }}/images/f1.jpg" title="ad image" alt="" />
-							</div>
-							<div class="featured-ad-right">
-								<h4>Lorem Ipsum is simply dummy text of the printing industry</h4>
-								<p>$ 450</p>
-							</div>
-							<div class="clearfix"></div>
-						</a>
-					</div>
-					<div class="featured-ad">
-						<a href="single.html">
-							<div class="featured-ad-left">
-								<img src="{{ url('/') }}/images/f2.jpg" title="ad image" alt="" />
-							</div>
-							<div class="featured-ad-right">
-								<h4>Lorem Ipsum is simply dummy text of the printing industry</h4>
-								<p>$ 380</p>
-							</div>
-							<div class="clearfix"></div>
-						</a>
-					</div>
-					<div class="featured-ad">
-						<a href="single.html">
-							<div class="featured-ad-left">
-								<img src="{{ url('/') }}/images/f3.jpg" title="ad image" alt="" />
-							</div>
-							<div class="featured-ad-right">
-								<h4>Lorem Ipsum is simply dummy text of the printing industry</h4>
-								<p>$ 560</p>
-							</div>
-							<div class="clearfix"></div>
-						</a>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-				</div>
-				<div class="ads-display col-md-9">
-					<div class="wrapper">					
-					<div class="bs-example bs-example-tabs" role="tabpanel" data-example-id="togglable-tabs">
-					  <ul id="myTab" class="nav nav-tabs nav-tabs-responsive" role="tablist">
-						<li role="presentation" class="active">
-						  <a href="#home" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">
-							<span class="text">Todos os Anúncios</span>
-						  </a>
-						</li>
-						<!-- <li role="presentation" class="next">
-						  <a href="#profile" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile">
-							<span class="text">Anúncios </span>
-						  </a>
-						</li>
-						<li role="presentation">
-						  <a href="#samsa" role="tab" id="samsa-tab" data-toggle="tab" aria-controls="samsa">
-							<span class="text">Company</span>
-						  </a>
-						</li> -->
-					  </ul>
-					  <div id="myTabContent" class="tab-content">
-						<div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledby="home-tab">
-						   <div>
-												<div id="container">
-								<div class="view-controls-list" id="viewcontrols">
-									<label>Visualizar :</label>
-									<a class="gridview"><i class="glyphicon glyphicon-th"></i></a>
-									<a class="listview active"><i class="glyphicon glyphicon-th-list"></i></a>
-								</div>
-								<div class="sort">
-								   <div class="sort-by">
-										<label>Ordenar por : </label>
-										<select>
-														<option value="">Mais recente</option>
-														<option value="">Mais Antigos</option>
-														<option value="">Menor Preço</option>
-														<option value="">Maior Preço</option>
-										</select>
-									   </div>
-									 </div>
-								<div class="clearfix"></div>
-							<ul class="list">
-								@forelse($anuncios as $anuncio)
-								<a href="{{ url('/anuncio/show') }}/{{ $anuncio->id }}">
-									<li>
-									<img src="{{ url('/') }}/images/m1.jpg" title="" alt="" />
-									<section class="list-left">
-									<h5 class="title">{{ $anuncio->titulo }}</h5>
-									<span class="adprice">{{ $anuncio->valor }}</span>
-									<p class="catpath">{{ $anuncio->categoria->nome }}</p>
-									</section>
-									<section class="list-right">
-									<span class="date">{{ $anuncio->created_at}}</span>
-									<span class="cityname">{{ $anuncio->cidade->nome }}</span>
-									</section>
-									<div class="clearfix"></div>
-									</li> 
-								</a>
-								@empty
-									<a>Nenhum Anúncio Encontrado</a>
-								@endforelse
-								
-							</ul>
-						</div>
-							</div>
-						</div>						
-						
-						<!--<ul class="pagination pagination-sm">
-							<li><a href="#">Anterior</a></li>
-							<li><a href="#">1</a></li>
-							<li><a href="#">2</a></li>
-							<li><a href="#">3</a></li>
-							<li><a href="#">4</a></li>
-							<li><a href="#">5</a></li>
-							<li><a href="#">6</a></li>
-							<li><a href="#">7</a></li>
-							<li><a href="#">8</a></li>
-							<li><a href="#">Próxima</a></li>
-						</ul>-->
-					  </div>
-					</div>
-				</div>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>	
-	</div>
-	<!-- // Mobiles -->
+@extends('layouts.portal')
+
+
+
+@section('scripts')	
+
+<!-- js -->
+
+<script type="text/javascript" src="{{ url('/') }}/js/jquery.min.js"></script>
+
+<!-- js -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+<script src="{{ url('/') }}/js/bootstrap.min.js"></script>
+
+<script src="{{ url('/') }}/js/bootstrap-select.js"></script>
+
+<script>
+
+  $(document).ready(function () {
+
+    var mySelect = $('#first-disabled2');
+
+
+
+    $('#special').on('click', function () {
+
+      mySelect.find('option:selected').prop('disabled', true);
+
+      mySelect.selectpicker('refresh');
+
+    });
+
+
+
+    $('#special2').on('click', function () {
+
+      mySelect.find('option:disabled').prop('disabled', false);
+
+      mySelect.selectpicker('refresh');
+
+    });
+
+
+
+    $('#basic2').selectpicker({
+
+      liveSearch: true,
+
+      maxOptions: 1
+
+    });
+
+  });
+
+</script>
+
+<script type="text/javascript" src="{{ url('/') }}/js/jquery.leanModal.min.js"></script>
+
+<link href="{{ url('/') }}/css/jquery.uls.css" rel="stylesheet"/>
+
+<link href="{{ url('/') }}/css/jquery.uls.grid.css" rel="stylesheet"/>
+
+<link href="{{ url('/') }}/css/jquery.uls.lcd.css" rel="stylesheet"/>
+
+<!-- Source -->
+
+<script src="{{ url('/') }}/js/jquery.uls.data.js"></script>
+
+<script src="{{ url('/') }}/js/jquery.uls.data.utils.js"></script>
+
+<script src="{{ url('/') }}/js/jquery.uls.lcd.js"></script>
+
+<script src="{{ url('/') }}/js/jquery.uls.languagefilter.js"></script>
+
+<script src="{{ url('/') }}/js/jquery.uls.regionfilter.js"></script>
+
+<script src="{{ url('/') }}/js/jquery.uls.core.js"></script>
+
+<script>
+
+			$( document ).ready( function() {
+
+				$( '.uls-trigger' ).uls( {
+
+					onSelect : function( language ) {
+
+						var languageName = $.uls.data.getAutonym( language );
+
+						$( '.uls-trigger' ).text( languageName );
+
+					},
+
+					quickList: ['en', 'hi', 'he', 'ml', 'ta', 'fr'] //FIXME
+
+				} );
+
+			} );
+
+		</script>
+
+    <script src="{{ url('/') }}/js/tabs.js"></script>
+
+	
+
+<script type="text/javascript">
+
+$(document).ready(function () {    
+
+var elem=$('#container ul');      
+
+	$('#viewcontrols a').on('click',function(e) {
+
+		if ($(this).hasClass('gridview')) {
+
+			elem.fadeOut(1000, function () {
+
+				$('#container ul').removeClass('list').addClass('grid');
+
+				$('#viewcontrols').removeClass('view-controls-list').addClass('view-controls-grid');
+
+				$('#viewcontrols .gridview').addClass('active');
+
+				$('#viewcontrols .listview').removeClass('active');
+
+				elem.fadeIn(1000);
+
+			});						
+
+		}
+
+		else if($(this).hasClass('listview')) {
+
+			elem.fadeOut(1000, function () {
+
+				$('#container ul').removeClass('grid').addClass('list');
+
+				$('#viewcontrols').removeClass('view-controls-grid').addClass('view-controls-list');
+
+				$('#viewcontrols .gridview').removeClass('active');
+
+				$('#viewcontrols .listview').addClass('active');
+
+				elem.fadeIn(1000);
+
+			});									
+
+		}
+
+	});
+
+});
+
+</script>
+
+
+
+@endsection
+
+
+
+@section('banner')
+
+	<div class="banner text-center">
+
+	  <div class="container">    
+
+			<h1>Lorem ipsum   <span class="segment-heading">    dolor amet </span> lorem ipsum</h1>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+			<a href="{{ url('/anuncio/create') }}">Criar Anúncio</a>
+
+	  </div>
+
+	</div>
+
+@endsection
+
+
+
+@section('content')
+
+	<!-- Mobiles -->
+
+	<div class="total-ads main-grid-border">
+
+		<div class="container">
+
+			<div class="select-box">
+
+				<div width="100%">
+
+					<div class="search" width="100%">
+
+						<div id="custom-search-input" width="100%">
+
+						<div class="input-group" width="100%">
+
+							<input type="text" class="form-control input-lg" placeholder="Buscar" width="100%" />
+
+							<span class="input-group-btn">
+
+								<button class="btn btn-info btn-lg" type="button">
+
+									<i class="glyphicon glyphicon-search"></i>
+
+								</button>
+
+							</span>
+
+						</div>
+
+					</div>
+
+					</div>
+
+				</div>
+
+				<div class="clearfix"></div>
+
+			</div>
+
+			<ol class="breadcrumb" style="margin-bottom: 5px;">
+
+			  <li><a href="{{ url('/') }}">Home</a></li>
+
+			  <li><a href="{{ url('/categoria') }}">Categorias</a></li>
+
+			  <li class="active">
+
+			     @if(isset($bread))
+
+			         {{ $bread }}
+
+			     @endif
+
+			      
+
+			  </li>
+
+			</ol>
+
+			<div class="ads-grid">
+
+				<div class="side-bar col-md-3">	
+
+				<div class="featured-ads">
+
+					<h2 class="sear-head fer">Anúncios Relacionados</h2>
+
+					<div class="featured-ad">
+
+						<a href="single.html">
+
+							<div class="featured-ad-left">
+
+								<img src="{{ url('/') }}/images/f1.jpg" title="ad image" alt="" />
+
+							</div>
+
+							<div class="featured-ad-right">
+
+								<h4>Lorem Ipsum is simply dummy text of the printing industry</h4>
+
+								<p>$ 450</p>
+
+							</div>
+
+							<div class="clearfix"></div>
+
+						</a>
+
+					</div>
+
+					<div class="featured-ad">
+
+						<a href="single.html">
+
+							<div class="featured-ad-left">
+
+								<img src="{{ url('/') }}/images/f2.jpg" title="ad image" alt="" />
+
+							</div>
+
+							<div class="featured-ad-right">
+
+								<h4>Lorem Ipsum is simply dummy text of the printing industry</h4>
+
+								<p>$ 380</p>
+
+							</div>
+
+							<div class="clearfix"></div>
+
+						</a>
+
+					</div>
+
+					<div class="featured-ad">
+
+						<a href="single.html">
+
+							<div class="featured-ad-left">
+
+								<img src="{{ url('/') }}/images/f3.jpg" title="ad image" alt="" />
+
+							</div>
+
+							<div class="featured-ad-right">
+
+								<h4>Lorem Ipsum is simply dummy text of the printing industry</h4>
+
+								<p>$ 560</p>
+
+							</div>
+
+							<div class="clearfix"></div>
+
+						</a>
+
+					</div>
+
+					<div class="clearfix"></div>
+
+				</div>
+
+				</div>
+
+				<div class="ads-display col-md-9">
+
+					<div class="wrapper">					
+
+					<div class="bs-example bs-example-tabs" role="tabpanel" data-example-id="togglable-tabs">
+
+					  <ul id="myTab" class="nav nav-tabs nav-tabs-responsive" role="tablist">
+
+						<li role="presentation" class="active">
+
+						  <a href="#home" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">
+
+							<span class="text">Todos os Anúncios</span>
+
+						  </a>
+
+						</li>
+
+						<!-- <li role="presentation" class="next">
+
+						  <a href="#profile" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile">
+
+							<span class="text">Anúncios </span>
+
+						  </a>
+
+						</li>
+
+						<li role="presentation">
+
+						  <a href="#samsa" role="tab" id="samsa-tab" data-toggle="tab" aria-controls="samsa">
+
+							<span class="text">Company</span>
+
+						  </a>
+
+						</li> -->
+
+					  </ul>
+
+					  <div id="myTabContent" class="tab-content">
+
+						<div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledby="home-tab">
+
+						   <div>
+
+												<div id="container">
+
+								<div class="view-controls-list" id="viewcontrols">
+
+									<label>Visualizar :</label>
+
+									<a class="gridview"><i class="glyphicon glyphicon-th"></i></a>
+
+									<a class="listview active"><i class="glyphicon glyphicon-th-list"></i></a>
+
+								</div>
+
+								<div class="sort">
+
+								   <div class="sort-by">
+
+										<label>Ordenar por : </label>
+
+										<select>
+
+														<option value="">Mais recente</option>
+
+														<option value="">Mais Antigos</option>
+
+														<option value="">Menor Preço</option>
+
+														<option value="">Maior Preço</option>
+
+										</select>
+
+									   </div>
+
+									 </div>
+
+								<div class="clearfix"></div>
+
+							<ul class="list">
+
+								@forelse($anuncios as $anuncio)
+
+								<a href="{{ url('/anuncio/show') }}/{{ $anuncio->id }}">
+
+									<li>
+
+									@if($anuncio->imagens->count() > 0)
+										<img src="{{ url('/') }}/uploads/{{ $anuncio->imagens[0]->nome }}" title="" alt="" />
+									@else
+										<img src="{{ url('/') }}/images/m1.jpg" title="" alt="" />
+									@endif
+
+									<section class="list-left">
+
+									<h5 class="title">{{ $anuncio->titulo }}</h5>
+
+									<span class="adprice">{{ $anuncio->valor }}</span>
+
+									<p class="catpath">{{ $anuncio->categoria->nome }}</p>
+
+									</section>
+
+									<section class="list-right">
+
+									<span class="date">{{ $anuncio->created_at}}</span>
+
+									<span class="cityname">{{ $anuncio->cidade->nome }}</span>
+
+									</section>
+
+									<div class="clearfix"></div>
+
+									</li> 
+
+								</a>
+
+								@empty
+
+									<a>Nenhum Anúncio Encontrado</a>
+
+								@endforelse
+
+								
+
+							</ul>
+
+						</div>
+
+							</div>
+
+						</div>						
+
+						
+
+						<!--<ul class="pagination pagination-sm">
+
+							<li><a href="#">Anterior</a></li>
+
+							<li><a href="#">1</a></li>
+
+							<li><a href="#">2</a></li>
+
+							<li><a href="#">3</a></li>
+
+							<li><a href="#">4</a></li>
+
+							<li><a href="#">5</a></li>
+
+							<li><a href="#">6</a></li>
+
+							<li><a href="#">7</a></li>
+
+							<li><a href="#">8</a></li>
+
+							<li><a href="#">Próxima</a></li>
+
+						</ul>-->
+
+					  </div>
+
+					</div>
+
+				</div>
+
+				</div>
+
+				<div class="clearfix"></div>
+
+			</div>
+
+		</div>	
+
+	</div>
+
+	<!-- // Mobiles -->
+
 	@endsection
